ae09f7681b9f021afdc5a17e5812e255e35a7c1f
Inseong <tomas1997@naver.com>
2023-03-26 16:46:23 +0900

    Simple Diary 2(Data)

--- React/simplediary/src/App.js
+++ React/simplediary/src/App.js
@@ -1,10 +1,44 @@
-import './App.css';
-import DiaryEditor from './DiaryEditor';
+import { useRef, useState } from "react";
+import "./App.css";
+import DiaryEditor from "./DiaryEditor";
+import DiaryList from "./DiaryList";
 
 function App() {
+  const [data, setData] = useState([]);
+
+  const dataId = useRef(0);
+
+  const onCreate = (author, content, emotion) => {
+    const created_date = new Date().getTime();
+    const newItem = {
+      author,
+      content,
+      emotion,
+      created_date,
+      id: dataId.current,
+    };
+    dataId.current += 1;
+    setData([newItem, ...data]);
+  };
+
+  const onRemove = (targetId) => {
+    console.log("${targetId}가 삭제되었습니다.");
+    const newDiaryList = data.filter((it) => it.id != targetId);
+    setData(newDiaryList);
+  };
+
+  const onEdit = (targetId, newContent) => {
+    setData(
+      data.map((it) =>
+        it.id === targetId ? { ...it, content: newContent } : it
+      )
+    );
+  };
+
   return (
     <div className="App">
-      <DiaryEditor/>
+      <DiaryEditor onCreate={onCreate} />
+      <DiaryList onRemove={onRemove} diaryList={data} />
     </div>
   );
 }
